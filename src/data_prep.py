1c24698f8e656bbe0deffe5af19d0d73520438c5
martingrzzler <martin.gressler@code.berlin>
2022-06-05 19:09:51 +0200

    normalization

--- src/data_prep.py
+++ src/data_prep.py
@@ -27,3 +27,19 @@ def str_column_to_int(dataset, column):
 		row[column] = lookup[row[column]]
 	
 	return lookup
+
+def minmax(dataset):
+	minmax = list()
+
+	for c in range(len(dataset[0])):
+		col_values = [row[c] for row in dataset]
+		col_min = min(col_values)
+		col_max = max(col_values)
+		minmax.append([col_min, col_max])
+	return minmax
+
+def normalize(dataset):
+	mm = minmax(dataset)
+	for row in dataset:
+		for i in range(len(row)):
+			row[i] = (row[i] - mm[i][0]) / (mm[i][1] - mm[i][0])
