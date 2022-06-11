795e1258b5f7b0a9193d9a929574a609ba90e219
martingrzzler <martin.gressler@code.berlin>
2022-06-11 12:53:14 +0200

    baseline algs

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
 from random import seed
-from data_prep import confusion_matrix, mean_absolute_error, print_confusion_matrix, root_mean_squared_error 
+from data_prep import RandomModel, Type, ZeroRuleModel, confusion_matrix, mean_absolute_error, print_confusion_matrix, root_mean_squared_error 
 seed(1)
 
-actual = [0.1, 0.2, 0.3, 0.4, 0.5]
-predicted = [0.11, 0.19, 0.29, 0.41, 0.5]
-mae = root_mean_squared_error(actual, predicted)
-print(mae)
+train = [[10], [15], [12], [15], [18], [20]]
+test = [[None], [None], [None], [None]]
 
+
+model = ZeroRuleModel()
+model.fit(train)
+print(model.predict(test, Type.REGRESSION))
 
